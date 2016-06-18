f5f11fceaac58ee70e0d74320557a5158ed60155
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-18 21:06:50 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,6 +3,5 @@
 class role_base::client-openvpn {
 
   create_resources( 'openvpn_client::client', $role_openvpn::openvpn_client_hash )
-  create_resources( 'file', $role_openvpn::file_hash )
-  
+
 }

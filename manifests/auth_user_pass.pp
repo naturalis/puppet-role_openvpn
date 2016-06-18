4f274635badefa49c1092e87bcf3f0412cf332d7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-18 21:06:22 +0200

    Update auth_user_pass.pp

--- manifests/auth_user_pass.pp
+++ manifests/auth_user_pass.pp
@@ -2,6 +2,9 @@
 #
 class role_base::auth_user_pass {
 
-  create_resources( 'file', $role_openvpn::auth_user_pass_hash )
+  $auth_user_pass_defaults = { 'path' => '/etc/openvpn/auth-user-pass',
+                             }
+
+  create_resources( 'file', $role_openvpn::auth_user_pass_hash, auth_user_pass_defaults )
   
 }
